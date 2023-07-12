bd2c16ea01cf399bb420bb2c42c023a69642da9f
Roberto Guerra <prosaka@hotmail.com>
2023-07-12 11:59:13 -0300

    feat: import google fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,29 @@
+import {
+  useFonts,
+  Archivo_700Bold,
+  Archivo_500Medium,
+  Archivo_400Regular,
+} from "@expo-google-fonts/archivo";
+
 import { HomeScreen } from "./src/screens/HomeScreen";
+import { SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
-  return <HomeScreen />;
+  let [fontsLoaded] = useFonts({
+    Archivo_700Bold,
+    Archivo_500Medium,
+    Archivo_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <HomeScreen />
+      </SafeAreaView>
+    </>
+  );
 }
